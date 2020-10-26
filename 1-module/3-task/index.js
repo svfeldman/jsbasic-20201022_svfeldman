310aa1caa1756b6f4aa81f0e0c399e90f5ccaaaf
Stanislav Feldman <stas.feldman@gmail.com>
2020-10-26 10:30:57 +0300

    написал функции к задачам 3,4 и 5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,4 +5,12 @@
  */
 function ucFirst(str) {
   // ваш код...
+  if (str != "undefined" && str.length >= 1) {
+    str = str[0].toUpperCase() + str.slice(1);
+    return str;
+  } else if (str.includes("''") != true) { 
+    return '';   
+  } else {
+    return false;
+  } 
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,10 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+  if (str.length > maxlength) {
+    str = str.slice(0, maxlength - 1) + "…";
+    return str;
+  } else {
+    return str;
+  }
 }
