abd6efc66ca1c9c094e380569efd0a4a4120b1ae
Stanislav Feldman <stas.feldman@gmail.com>
2020-11-14 22:25:28 +0300

    кнопка удаляет и возвращает текст

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
   // ваш код...
+  let bntText = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  bntText.addEventListener('click', () => {
+    if (text.hasAttribute('hidden') === false) {
+      text.setAttribute('hidden', true);
+    } else {
+      text.removeAttribute('hidden');
+    }
+  });
 }
