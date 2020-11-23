c5da07e651ef6cb055a880099f3950eb8fa8192e
Stanislav Feldman <stas.feldman@gmail.com>
2020-11-24 00:03:02 +0300

    ...

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,36 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },
  *
  * @constructor
  */
 export default class UserTable {
   constructor(rows) {
+    // for (let i = 0; i < rows.length; i++) {
+    //   let elem = rows[i];
+    //   console.log(this);
+    //   if (!this.elem) {
+    //     this.elem = document.createElement('table');
+    //     this.elem.innerHTML = `<thead>
+    //                               <tr>
+    //                                 <td>Имя</td>
+    //                                 <td>Возраст</td>
+    //                                 <td>Зарплата</td>
+    //                                 <td>Город</td>
+    //                                 <td>нажми меня :)</td>
+    //                               </tr>
+    //                           </thead>
+    //                           <tbody>
+    //                             <tr>
+    //                               <td>${elem.name}</td>
+    //                               <td>${elem.age}</td>
+    //                               <td>${elem.salary}</td>
+    //                               <td>${elem.city}</td>
+    //                               <td><button>X</button></td>
+    //                             </tr>
+    //                          </tbody>`;
+    //   }
+    // }
   }
 }
